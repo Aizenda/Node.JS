9cc8e37dee9cb54453d1a82ad82b4be6e0c53d58
Aizenda <147816053+Aizenda@users.noreply.github.com>
2024-09-17 12:15:31 +0800

    新增表單&連線方法學習

--- img & hyperlink.js
+++ img & hyperlink.js
@@ -4,19 +4,22 @@ const express = require('express');
 //cread Application objet
 const app=express();
 
-//將靜態檔案名稱對應到http://127.0.0.1:3000/檔案名稱
+//將靜態檔案名稱對應到http://127.0.0.1:3100檔案名稱
 app.use(express.static("public"));
 
 //設定樣板引擎
 app.set("view engine", "ejs");
 
 //設定session 
-
 const session=require("express-session");
+
 //設定樣板檔案資料夾位置
 app.set("views", "./views");
 
-//啟動伺服器在測試網址http://127.0.0.1:3000/
+//讓後端能夠接收POST方法的資料
+app.use(express.urlencoded({extended:true}));
+
+//啟動伺服器在測試網址http://127.0.0.1:3100/
 app.listen(3100, function (){
     console.log("Sever started");
 });
@@ -24,8 +27,8 @@ app.get("/",(req,res) =>{
     res.render("home.ejs");
 });
 //設定路徑，預期要求字串會是城市 =city
-app.get("/data",(req,res) =>{
-    const name=req.query.city;
+app.post("/data",(req,res) =>{
+    const name=req.body.city;
     if(name==="台北"){
         let data={pop:300, 城市名稱:name};
         res.render("city.ejs", data);
